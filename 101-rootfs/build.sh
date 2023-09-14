91a00dd6e6a0b15aae8eb7b535b8e17a39e50e56
gnuhub <gnuhub@gmail.com>
2023-09-14 19:56:34 +0800

    auto commit

--- 101-rootfs/build.sh
+++ 101-rootfs/build.sh
@@ -7,12 +7,5 @@ apt update
 export DEBIAN_FRONTEND=noninteractive
 
 apt install multistrap -y
-
-multistrap -f jammy.multistrap || true
-
-cp ubuntu-archive-keyring.gpg /jammy-rootfs/etc/apt/trusted.gpg.d
-
 multistrap -f jammy.multistrap
-
-cp sources.list /jammy-rootfs/etc/apt/sources.list
 
